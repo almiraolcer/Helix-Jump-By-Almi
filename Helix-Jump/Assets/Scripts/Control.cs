39586157d86f5c0a336170a0ade564f98daf1eca
Almira Ölçer <almi-olcer@hotmail.com>
2022-06-21 23:41:22 +0300

    Rotation of Cylinder is Added
    Cylinder rotates when Left and Right Arrow button is pressed. New input system is used.

---
+++ Helix-Jump/Assets/Scripts/Control.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Control : MonoBehaviour
+{
+
+    PlayerInput playerinput;
+    Vector3 inputdirection;
+    [SerializeField] float _rotationSpeed = 80f;
+
+    private void Awake() {
+        
+        playerinput = new PlayerInput();
+
+        playerinput.CylinderControl.Rotation.performed += x  => RotateCylinder(x.ReadValue<Vector2>());
+        playerinput.CylinderControl.Rotation.canceled += x  => RotateCylinder(x.ReadValue<Vector2>());
+    }
+
+    void RotateCylinder(Vector2 val){
+
+       inputdirection = new Vector3(0 , val.x , 0);
+        
+
+    }
+
+    private void Update() {
+
+        transform.Rotate(inputdirection * Time.deltaTime * _rotationSpeed);
+        
+    }
+
+    private void OnEnable(){
+    playerinput.CylinderControl.Enable();
+
+    }
+
+    private void OnDisable(){
+    playerinput.CylinderControl.Disable();
+        
+    }
+}
+
