5f7fd4f8d9f10d704eaf732c279aa3238c1b3a74
Almira Ölçer <almi-olcer@hotmail.com>
2022-06-26 16:43:27 +0300

    Exit, Restart Level and Next Level is added
    Level 2 is created. Scene Management is used do add Exit, Restart Level and Next Level options. Canvas is created for the Restart Screen and Finish Screen.

---
+++ Helix-Jump/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public GameObject RestartMenu;
+    public GameObject FinishMenu;
+    private Rigidbody rb;
+
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Trap")){
+            rb.isKinematic = true;
+            RestartMenu.SetActive(true);
+
+        }
+        else if(other.gameObject.CompareTag("FinishLine")){
+            FinishMenu.SetActive(true);
+            rb.isKinematic = true;
+
+        }
+
+    }
+
+    public void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel(){
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+
+    
+   
+}
