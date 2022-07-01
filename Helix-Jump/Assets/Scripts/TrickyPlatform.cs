c138480ab68cb9f560534690415a11f6c3f803c9
Almira Ölçer <almi-olcer@hotmail.com>
2022-07-01 17:17:33 +0300

    Sound for TrickyPlatform is Added

--- Helix-Jump/Assets/Scripts/TrickyPlatform.cs
+++ Helix-Jump/Assets/Scripts/TrickyPlatform.cs
@@ -7,15 +7,17 @@ public class TrickyPlatform : MonoBehaviour
 {
     private float _duration=3f;
     public Transform target;
-    
+
+    [SerializeField]  AudioSource Tricksource;
+    [SerializeField]  AudioClip  Trickclip;
 
 
     void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("TrickyPlatform")){
-
             GameObject otherObj = other.gameObject; 
             otherObj.transform.DOMoveY(target.position.y,_duration);
+            Tricksource.PlayOneShot(Trickclip);
         }
     }
 }
